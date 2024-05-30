0425ccfe2b908c35b75515b5bc1e85626c1b5ef5
Mauros Milach Junior <mauros99milach@gmail.com>
2024-05-30 19:18:17 -0300

    feat: Created Service layer and component annotation

---
+++ src/main/java/com/whebsys/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.whebsys.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.whebsys.course.entities.User;
+import com.whebsys.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
