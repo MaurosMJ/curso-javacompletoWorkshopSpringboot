96ce5ae0da6311686950549f20c7b4636f6dc27a
Mauros Milach Junior <mauros99milach@gmail.com>
2024-05-31 10:34:58 -0300

    feat: created OrderRepository, OrderResource, OrderService and ISO 8601 Pattern

--- src/main/java/com/whebsys/course/CourseApplication.java
+++ src/main/java/com/whebsys/course/CourseApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApplication {
-
-	public static void main(String[] args) {
+public class CourseApplication  {
+	
+	public static void main(String[] args)  {
 		SpringApplication.run(CourseApplication.class, args);
-		System.out.println("###############################################@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		
+
 	}
 
 }
