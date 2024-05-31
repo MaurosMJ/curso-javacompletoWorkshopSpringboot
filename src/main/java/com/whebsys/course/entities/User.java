f0d105a617643ec62fd5484701ba7291c4a062d3
Mauros Milach Junior <mauros99milach@gmail.com>
2024-05-30 22:26:37 -0300

    feat: Created class Order

--- src/main/java/com/whebsys/course/entities/User.java
+++ src/main/java/com/whebsys/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.whebsys.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +27,10 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany (mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+
 	public User(Long id, String name, String email, String phone, String password){
 		this.id = id;
 		this.name = name;
@@ -34,6 +41,10 @@ public class User implements Serializable {
 	
 	public User(){
 
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public Long getId() {
