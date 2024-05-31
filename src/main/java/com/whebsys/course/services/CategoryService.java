721f38d7e990b490cde96988445a14bc9feb8ed0
Mauros Milach Junior <mauros99milach@gmail.com>
2024-05-31 18:34:56 -0300

    feat: created category entity

---
+++ src/main/java/com/whebsys/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.whebsys.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.whebsys.course.entities.Category;
+import com.whebsys.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
