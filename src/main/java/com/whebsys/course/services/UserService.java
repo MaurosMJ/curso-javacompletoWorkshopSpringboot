1974bafcab59e6ba5f9f2a5b5c457a19468f9961
Mauros Milach Junior <mauros99milach@gmail.com>
2024-06-02 19:32:44 -0300

    Exception handling - findById

--- src/main/java/com/whebsys/course/services/UserService.java
+++ src/main/java/com/whebsys/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.whebsys.course.entities.User;
 import com.whebsys.course.repositories.UserRepository;
+import com.whebsys.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert (User obj) {
